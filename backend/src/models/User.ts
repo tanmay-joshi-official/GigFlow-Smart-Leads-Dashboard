5401e5d7e4886c691cb4118f844c074f7483203f
Tanmay Joshi <tanmayjoshi579@gmail.com>
2026-05-18 23:26:29 +0530

    Fix: Remove next() from async pre-save middleware to fix TS2349 error on Render

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -42,9 +42,8 @@ const userSchema = new mongoose.Schema<IUser>(
 );
 
 // Encrypt password using bcrypt
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function () {
   if (!this.isModified('password')) {
-    next();
     return;
   }
 
